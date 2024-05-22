8865d8e0e47761c75c947c5c802cfc95f16cf1cb
qkrclgh2 <qkrclgh2@gmail.com>
2024-05-22 21:45:19 +0900

    5월 21일 todolist 리팩토링  시작

---
+++ workspace/5_vue-app2/1_prop-test/src/App4.vue
@@ -0,0 +1,29 @@
+<template lang="">
+  <h1>유저 정보 조회</h1>
+  <ul>
+    <CheckboxItem v-for='user in users' :key='user.number' :user='user' />
+  </ul>
+</template>
+<script>
+import CheckboxItem from './components/CheckboxItem4.vue';
+import User from './user.js';
+
+  export default {
+    name : 'App4',
+    components : {CheckboxItem},
+
+    data() {
+      return {
+        users : [
+          new User(1, '류호근', true),
+          new User(2, '홍길동', true),
+          new User(3, '강감찬', true),
+          new User(4, '신짱구')
+        ]
+      }
+    },
+  }
+</script>
+<style lang="">
+  
+</style>
--- workspace/5_vue-app2/1_prop-test/src/main.js
+++ workspace/5_vue-app2/1_prop-test/src/main.js
@@ -1,10 +1,10 @@
 // import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import App2 from './App2.vue'
-import App3 from './App3.vue'
+// import App from './App.vue'
+// import App2 from './App2.vue'
+// import App3 from './App3.vue'
+import App from './App4.vue'
 
-// createApp(App).mount('#app')
-// createApp(App2).mount('#app')
-createApp(App3).mount('#app')
+
+createApp(App).mount('#app')
